438546fa5f39ed721358b6b44634367c98aff059
Pereverza Tetiana <tanyusha.vzletka82@gmail.com>
2025-04-21 17:10:53 +0300

    Initial commit

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,69 @@
+import { useDispatch, useSelector } from "react-redux";
+import { addContact } from "../../redux/contactsSlice";
+import { nanoid } from "nanoid";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import css from "./ContactForm.module.css";
+
+const initialValues = {
+  name: "",
+  number: "",
+};
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().min(3).max(50).required("Required"),
+  number: Yup.string().min(3).max(20).required("Required"),
+});
+
+function ContactForm() {
+  const dispatch = useDispatch();
+  const contacts = useSelector((state) => state.contacts.items);
+
+  const handleSubmit = (values, actions) => {
+    const isDuplicate = contacts.some(
+      (contact) => contact.name.toLowerCase() === values.name.toLowerCase()
+    );
+
+    if (isDuplicate) {
+      actions.resetForm();
+      return;
+    }
+
+    const newContact = {
+      id: nanoid(),
+      name: values.name,
+      number: values.number,
+    };
+
+    dispatch(addContact(newContact));
+    actions.resetForm();
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      <Form className={css.form}>
+        <label className={css.label}>
+          Name
+          <Field className={css.input} type="text" name="name" />
+          <ErrorMessage name="name" component="div" className={css.error} />
+        </label>
+
+        <label className={css.label}>
+          Number
+          <Field className={css.input} type="tel" name="number" />
+          <ErrorMessage name="number" component="div" className={css.error} />
+        </label>
+
+        <button type="submit" className={css.button}>
+          Add contact
+        </button>
+      </Form>
+    </Formik>
+  );
+}
+
+export default ContactForm;
